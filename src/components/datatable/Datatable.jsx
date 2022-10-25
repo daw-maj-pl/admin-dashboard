02294ae5dd9ee9b1f237b533b19d6109fbc30bbc
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-10-25 11:57:18 +0200

    Add column with action buttons

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -3,11 +3,26 @@ import { DataGrid } from '@mui/x-data-grid';
 import { userColumns, userRows } from '../../datatablesource';
 
 const Datatable = () => {
+  const actionColumn = [
+    {
+      field: 'action',
+      headerName: 'Action',
+      width: 200,
+      renderCell: params => {
+        return (
+          <div className="cellAction">
+            <div className="viewButton">View</div>
+            <div className="deleteButton">Delete</div>
+          </div>
+        );
+      }
+    }
+  ];
   return (
     <div className="datatable">
       <DataGrid
         rows={userRows}
-        columns={userColumns}
+        columns={userColumns.concat(actionColumn)}
         pageSize={9}
         rowsPerPageOptions={[9]}
         checkboxSelection
