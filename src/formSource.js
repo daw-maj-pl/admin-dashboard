fcf8b8ee0d53b6a8d4d83d9b1884432ca76a8853
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-10-28 19:10:11 +0200

    Create formSource file with userInputs

---
+++ src/formSource.js
@@ -0,0 +1,43 @@
+export const userInputs = [
+  {
+    id: 1,
+    label: 'Username',
+    type: 'text',
+    placeholder: 'john_doe'
+  },
+  {
+    id: 2,
+    label: 'Name and surname',
+    type: 'text',
+    placeholder: 'John Doe'
+  },
+  {
+    id: 3,
+    label: 'Email',
+    type: 'email',
+    placeholder: 'john_doe@gmail.com'
+  },
+  {
+    id: 4,
+    label: 'Phone',
+    type: 'text',
+    placeholder: '+1 123 456 7890'
+  },
+  {
+    id: 5,
+    label: 'Password',
+    type: 'password'
+  },
+  {
+    id: 6,
+    label: 'Address',
+    type: 'text',
+    placeholder: '3737 Mahlon St. Newark, NJ 07102'
+  },
+  {
+    id: 7,
+    label: 'Country',
+    type: 'text',
+    placeholder: 'USA'
+  }
+];
