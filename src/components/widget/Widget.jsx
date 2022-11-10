2c6cc72d105cd86ad88b60da1cd08fed5204cf59
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-11-10 20:03:13 +0100

    Fetch data using firebase queries

--- src/components/widget/Widget.jsx
+++ src/components/widget/Widget.jsx
@@ -1,16 +1,21 @@
-import './widget.scss';
+import { useEffect, useState } from 'react';
+import { collection, query, where, getDocs } from 'firebase/firestore';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
 import AccountBalanceWalletOutlinedIcon from '@mui/icons-material/AccountBalanceWalletOutlined';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import MonetizationOnOutlinedIcon from '@mui/icons-material/MonetizationOnOutlined';
+import './widget.scss';
+import { db } from '../../firebase';
 
 const Widget = ({ type }) => {
+  const [amount, setAmount] = useState(null);
+  const [diff, setDiff] = useState(null);
   let data;
 
   //temporary
-  const amount = 120;
-  const diff = 24;
+  // const amount = 120;
+  // const diff = 24;
 
   switch (type) {
     case 'user':
@@ -77,6 +82,36 @@ const Widget = ({ type }) => {
     default:
       break;
   }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const today = new Date();
+      const lastMonth = new Date(new Date().setMonth(today.getMonth() - 1));
+      const prevMonth = new Date(new Date().setMonth(today.getMonth() - 2));
+
+      const lastMonthQuery = query(
+        collection(db, 'users'),
+        where('timeStamp', '<=', today),
+        where('timeStamp', '>', lastMonth)
+      );
+      const prevMonthQuery = query(
+        collection(db, 'users'),
+        where('timeStamp', '<=', lastMonth),
+        where('timeStamp', '>', prevMonth)
+      );
+
+      const lastMonthData = await getDocs(lastMonthQuery);
+      const prevMonthData = await getDocs(prevMonthQuery);
+
+      setAmount(lastMonthData.docs.length);
+      setDiff(
+        ((lastMonthData.docs.length - prevMonthData.docs.length) /
+          prevMonthData.docs.length) *
+          100
+      );
+    };
+    fetchData();
+  }, []);
 
   return (
     <div className="widget">
